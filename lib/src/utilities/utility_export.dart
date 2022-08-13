ef30c35692ce3ae229b7891738f339a8116f4daf
Arun Prakash <arun.prakash.456789@gmail.com>
2022-08-14 00:16:23 +0530

    misc

--- lib/src/utilities/utility_export.dart
+++ lib/src/utilities/utility_export.dart
@@ -1,4 +1,9 @@
 export 'helpers.dart'
-    show MapExtensions, deserialize, serialize, parseDateIfNotNull;
+    show
+        MapExtensions,
+        deserialize,
+        serialize,
+        parseDateIfNotNull,
+        getMIMETypeFromExtension;
 export 'response_extensions/post_extensions.dart';
 export 'wordpress_callback.dart';
