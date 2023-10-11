4e99a7906c1f54285f904c5d9439cc5c95393f7f
Arun Prakash <arun.prakash.456789@gmail.com>
2023-10-12 00:49:11 +0530

    misc

--- lib/src/interface/page.dart
+++ lib/src/interface/page.dart
@@ -4,8 +4,8 @@ import '../responses/page_response.dart';
 /// Represents the page interface.
 final class PageInterface extends IRequestInterface
     with
-        CreateOperation<Page, CreatePostRequest>,
-        DeleteOperation<DeletePostRequest>,
-        RetriveOperation<Page, RetrivePostRequest>,
-        UpdateOperation<Page, UpdatePostRequest>,
-        ListOperation<Page, ListPostRequest> {}
+        CreateOperation<Page, CreatePageRequest>,
+        DeleteOperation<DeletePageRequest>,
+        RetriveOperation<Page, RetrivePageRequest>,
+        UpdateOperation<Page, UpdatePageRequest>,
+        ListOperation<Page, ListPageRequest> {}
